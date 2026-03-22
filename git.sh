ea10a9e21cc6e74fdfbd51045d77548f9531cda8
Nochang0 <hy040504@kakao.com>
2026-03-22 16:56:57 +0900

    2Grade Web-Programming1 update (2026-03-22 16:56:57)

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-read -p "커밋 메시지: " msg
+read -p "(웹 프로그래밍1) 커밋 메시지: " msg
 
 # 입력이 비었으면 기본 메시지 사용
 if [ -z "$msg" ]; then
-    msg="2Grade Web-Programming1 update"
+    msg="2Grade Web-Programming1 update ($(date '+%Y-%m-%d %H:%M:%S'))"
 fi
 
 git add .
